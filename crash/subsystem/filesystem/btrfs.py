d90ce86e63515086c13964076202cbce87d159e3
Jeff Mahoney <jeffm@suse.com>
2019-04-29 15:08:53 -0400

    crash.subsystem.filesystem.btrfs: add btrfs_fsid and btrfs_metadata_uuid
    This adds helpers to export the fsid and metadata uuid from btrfs file
    systems.
    
    Signed-off-by: Jeff Mahoney <jeffm@suse.com>

--- crash/subsystem/filesystem/btrfs.py
+++ crash/subsystem/filesystem/btrfs.py
@@ -3,10 +3,11 @@
 
 import gdb
 
+from crash.util import decode_uuid, struct_has_member
 from crash.util.symbols import Types
 
 types = Types([ 'struct btrfs_inode', 'struct btrfs_fs_info *',
-                  'struct btrfs_fs_info' ])
+                'struct btrfs_fs_info' ])
 
 def btrfs_inode(vfs_inode):
     """
@@ -38,3 +39,18 @@ def btrfs_fs_info(super_block):
             btrfs_fs_info
     """
     return super_block['s_fs_info'].cast(types.btrfs_fs_info_p_type)
+
+def btrfs_fsid(sb):
+    fs_info = btrfs_fs_info(sb)
+    if struct_has_member(types.btrfs_fs_info_type, 'fsid'):
+        return decode_uuid(fs_info['fsid'])
+    return decode_uuid(fs_info['fs_devices']['fsid'])
+
+def btrfs_metadata_uuid(sb):
+    fs_info = btrfs_fs_info(sb)
+    if struct_has_member(types.btrfs_fs_info_type, 'metadata_uuid'):
+        return decode_uuid(fs_info['metadata_uuid'])
+    elif struct_has_member(fs_info['fs_devices'].type, 'metadata_uuid'):
+        return decode_uuid(fs_info['fs_devices']['metadata_uuid'])
+    else:
+        return btrfs_fsid(sb)
